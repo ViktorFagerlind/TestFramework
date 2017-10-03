4d4218e72da384eaf862ca5c9be6b7d659c369de
Viktor Faegerlind <viktor_fagerlind@hotmail.com>
2017-10-03 15:36:15 +0200

    Log experiment

--- tests/TestB.py
+++ tests/TestB.py
@@ -1,12 +1,26 @@
-from Logging  import Log
+import sys
+sys.path.insert(0, "..\\bin")
+
 from Test import Test
 
+import logging
+
 class TestB (Test):
-  def __init__ (self, instanceName):
+  def __init__ (self, instanceName="Default"):
     Test.__init__(self, "Test B", instanceName)
            
   def runSequence (self):
-    self.printSubstep ("Sub test 1")
-        
+    logging.info('Started')
+    logging.warning('Running...')
+    logging.info('Stopped')
 
 
+if __name__ == '__main__':
+  test = TestB()
+  logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+  
+  fh = logging.FileHandler(filename=test.instanceName + '.log')
+  fh.setFormatter (logging.Formatter ('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+  logging.getLogger().addHandler(fh)
+  
+  test.runSequence()
